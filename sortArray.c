5700a82c3c23dea7ebb4d5d7c878e4ed6fc89346
mubarak-mustopha <mustophamubarack17@gmail.com>
2022-08-02 22:19:55 +0100

    A program that sorts a given array of integers in desecending order

---
+++ sortArray.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+int SearchTarget(int array[], int len, int target);
+
+int IndexOfMaxInRange(int array[], int length, int range)
+{
+    int i, max;
+    max = array[range];
+    for (i = range; i < length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return SearchTarget(array, length, max);
+}
+
+int SearchTarget(int array[], int len, int target)
+{
+    int i;
+    for (i = 0; i < len; i++)
+    {
+        if (array[i] == target)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void SwapElement(int array[], int index0, int index1)
+{
+    int id_x = array[index0];
+    int id_y = array[index1];
+    array[index0] = id_y;
+    array[index1] = id_x;
+}
+
+void PrintArray(int array[], int length)
+{
+    int i;
+    for (i = 0; i < length; i++)
+    {
+        printf("%i ", array[i]);
+    }
+}
+
+void SortArray(int array[], int length)
+{
+    int i;
+    for (i = 0; i < length; i++)
+    {
+        int max_index = IndexOfMaxInRange(array, length, i);
+        SwapElement(array, max_index, i);
+    }
+}
+
+int main(void)
+{
+    int arr[7] = {12, 8, 4, 9, 11, 90, 4};
+    int length = sizeof(arr) / sizeof(arr[0]);
+    PrintArray(arr, length);
+    printf("\n");
+    SortArray(arr, length);
+    PrintArray(arr, length);
+}
